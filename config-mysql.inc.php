ba5c263c62ecefa616d559605be9f59ed9286701
GLSteiner <c1ue00ster@gmail.com>
2017-01-19 12:34:22 +0100

    Update config-mysql.inc.php

--- config-mysql.inc.php
+++ config-mysql.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 // MYSQL VERBDINUNG
-$server = "dd29920.kasserver.com";
-$username = "d0220110";
-$password = "V7ntpLuc9HeKVYRw";
-$database = "d0220110";
+$server = "";
+$username = "";
+$password = "";
+$database = "";
