5269bb7c0d91d18f007d4707d1bb61f2d6324f92
GLSteiner <greg@10.0.0.2>
2017-05-08 21:03:38 +0200

    Alte Grafiken zu Git hinzugefugt

--- Desktop/scann.php
+++ Desktop/scann.php
@@ -10,6 +10,4 @@ echoifadmin(20);
     <b>Listenscan</b>
     Scan<input type="checkbox" name="Check" value="1" checked> <input type="submit" value="Go!"><br><br>
     <textarea name="IID" rows="25" style="width: 75%;" required=""></textarea>
-
-
 </form>
