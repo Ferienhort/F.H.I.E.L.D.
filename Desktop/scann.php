25c69023c7ff69b901f1ea4f8292501a14323be8
GLSteiner <greg@10.0.0.2>
2017-05-08 20:40:14 +0200

    Scann Bug Schnellfix

--- Desktop/scann.php
+++ Desktop/scann.php
@@ -9,7 +9,7 @@ echoifadmin(20);
 <form method="POST" action="omini.php">
     <b>Listenscan</b>
     Scan<input type="checkbox" name="Check" value="1" checked> <input type="submit" value="Go!"><br><br>
-    <textarea name="IID" rows="25" style="width: 75%;"></textarea>
+    <textarea name="IID" rows="25" style="width: 75%;" required=""></textarea>
 
 
 </form>
